704306e472c347cf6102e328a6c27babaa09f3b8
MahmoudSafaya <mahmoudsafaya7@gmail.com>
2021-02-12 15:22:55 +0200

    added more

--- js/script.js
+++ js/script.js
@@ -16,3 +16,20 @@ close.addEventListener('click', () => {
   menu.style.display = 'block';
   close.style.display = 'none';
 });
+
+
+
+//to top button code
+const toTop = document.querySelector('.toTop');
+
+window.addEventListener('scroll', () => {
+  if(window.pageYOffset > 400) {
+    toTop.style.display = 'block';
+  } else {
+    toTop.style.display = 'none';
+  }
+});
+
+toTop.addEventListener('click', () => {
+  window.scrollTo(0, 0);
+});
